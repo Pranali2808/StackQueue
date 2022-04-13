d24119916c456b31bf95c3c43e06285dbbbb3067
pranali2808 <ranepranali1997@gmail.com>
2022-04-13 14:00:40 +0530

    ability to create queue of 56 30 70

--- Program.cs
+++ Program.cs
@@ -13,23 +13,29 @@ namespace StackQueue
            Console.WriteLine("Welcome to Data Structures Programs");
             LinkedList linkedList = new LinkedList();
             Stack stack = new Stack();
+            Queue queue = new Queue();
             //stack.Push(70);
             //stack.Push(30);
             //stack.Push(56);
 
-            stack.Push(70);
-            stack.Push(30);
-            stack.Push(56);
-            stack.Display();
-            stack.Peek();
-            stack.Pop();
-            stack.Display();
-            stack.Peek();
-            stack.Pop();
-            stack.Display();
-            stack.Peek();
-            stack.Pop();
-            stack.Display();
+            //stack.Push(70);
+            //stack.Push(30);
+            //stack.Push(56);
+            //stack.Display();
+            //stack.Peek();
+            //stack.Pop();
+            //stack.Display();
+            //stack.Peek();
+            //stack.Pop();
+            //stack.Display();
+            //stack.Peek();
+            //stack.Pop();
+
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+            
+            queue.Display();
             Console.ReadLine();
         }
     }
