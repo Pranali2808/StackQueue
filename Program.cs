7404a25f6dfd987e92f705d13f81c513f81c093e
pranali2808 <ranepranali1997@gmail.com>
2022-04-13 13:24:23 +0530

    push the element in stack

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Welcome to Data Structures Programs");
+            LinkedList linkedList = new LinkedList();
+            Stack stack = new Stack();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);            
+            stack.Display();
+            Console.ReadLine();
+        }
+    }
+}
